f49a77acc31a1337b6b541c4e8ca2b04f09fd93c
Ken Wright <kenneth.gordon.wright@gmail.com>
2022-09-07 18:52:56 -0400

    feat: add modules and set up environment

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+	return (
+		<div className="App">
+			<header>
+				<h1>Survivor League</h1>
+			</header>
+		</div>
+	);
+};
+
+export default App;
